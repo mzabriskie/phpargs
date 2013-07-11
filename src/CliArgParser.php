39fae97d7862135302c0f9ff24c2d91711431ca0
mzabriskie <mzabriskie@gmail.com>
2013-07-11 08:50:35 -0600

    Adding source

---
+++ src/CliArgParser.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+
+Copyright (c) 2013 by Matt Zabriskie
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+ */
+
+class CliArgParser {
+    private $args;
+    private $extra;
+
+    function __construct($args) {
+        $this->args = array();
+        $this->extra = array();
+
+        $source = &$this->args;
+        for ($i=0; $i<sizeof($args); $i++) {
+            // Any args that come after -- are considered extra args
+            if ($args[$i] == '--') {
+                $source = &$this->extra;
+                continue;
+            }
+            $source[] = $args[$i];
+        }
+    }
+
+    function hasOption($short, $long = null) {
+        // Look for explicit options
+        if (!is_null($short) && in_array('-' . $short, $this->args)) return true;
+        if (!is_null($long) && in_array('--' . $long, $this->args)) return true;
+
+        // Look for joined short option (e.g., -rf)
+        for ($i=0; $i<sizeof($this->args); $i++) {
+            if ($this->args[$i]{1} != '-' && strpos($this->args[$i], $short) > -1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function getValue($short, $long = null) {
+        if (is_numeric($short)) {
+            if ($short > 0) return $this->getArg($short);
+            else return $this->getArg(sizeof($this->args) + $short);
+        } else {
+            $opts = array();
+            if (!is_null($short)) $opts[] = '-' . $short;
+            if (!is_null($long)) $opts[] = '--' . $long;
+
+            if (sizeof($opts) > 0) {
+                $value = array();
+
+                // Look for value(s) of option
+                for ($i=0; $i<sizeof($this->args); $i++) {
+                    if (in_array($this->args[$i], $opts) && $this->args[$i+1]{0} != '-') {
+                        $value[] = $this->args[++$i];
+                    }
+                }
+
+                if (sizeof($value) > 0) {
+                    return sizeof($value) == 1 ? $value[0] : $value;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    function getExtraArgs() {
+        return $this->extra;
+    }
+
+    private function getArg($index) {
+        if ($index < 0 || $index >= sizeof($this->args)) throw new InvalidArgumentException();
+        return $this->args[$index];
+    }
+}
---
+++ test/CliArgParserTest.php
@@ -0,0 +1,112 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../src/CliArgParser.php');
+
+class CliArgParserTest extends TestCase {
+    function testBooleanOptions() {
+        $parser = new CliArgParser(array('script.php', '-a', '-b', 'abc', '-c'));
+
+        $this->assertTrue($parser->hasOption('a'));
+        $this->assertTrue($parser->hasOption('c'));
+        $this->assertEquals('abc', $parser->getValue('b'));
+    }
+
+    function testNumberOptions() {
+        $parser = new CliArgParser(array('script.php', '-a', '12345', '-b', '123.45'));
+
+        $this->assertEquals(12345, $parser->getValue('a'));
+        $this->assertEquals(123.45, $parser->getValue('b'));
+    }
+
+    function testStringOptions() {
+        $parser = new CliArgParser(array('script.php', '-a', 'abcdefg', '-b', 'hijklmnop'));
+
+        $this->assertEquals('abcdefg', $parser->getValue('a'));
+        $this->assertEquals('hijklmnop', $parser->getValue('b'));
+    }
+
+    function testArrayOptions() {
+        $parser = new CliArgParser(array('script.php', '-a', 'a', '-a', 'b'));
+
+        $arr = $parser->getValue('a');
+        $this->assertEquals(2, sizeof($arr));
+        $this->assertEquals('a', $arr[0]);
+        $this->assertEquals('b', $arr[1]);
+    }
+
+    function testMixedArrayOptions() {
+        $parser = new CliArgParser(array('script.php', '-a', 'a', '-a', 'b', '-b', '--alpha', 'c'));
+
+        $arr = $parser->getValue('a', 'alpha');
+        $this->assertEquals(3, sizeof($arr));
+        $this->assertEquals('a', $arr[0]);
+        $this->assertEquals('b', $arr[1]);
+        $this->assertEquals('c', $arr[2]);
+    }
+
+    function testInvalidArrayOptions() {
+        $parser = new CliArgParser(array('script.php', '-a', 'a', '-a', '-b', '-a', 'c'));
+
+        $arr = $parser->getValue('a');
+        $this->assertEquals(2, sizeof($arr));
+        $this->assertEquals('a', $arr[0]);
+        $this->assertEquals('c', $arr[1]);
+    }
+
+    function testCombinedShortOptions() {
+        $parser = new CliArgParser(array('script.php', '-fb', 'foo'));
+
+        $this->assertTrue($parser->hasOption('f'));
+        $this->assertTrue($parser->hasOption('b'));
+    }
+
+    function testMultipleOptions() {
+        $parser = new CliArgParser(array('script.php', '-a', 'a', '--bravo', 'b'));
+
+        $this->assertEquals('a', $parser->getValue('a', 'alpha'));
+        $this->assertEquals('b', $parser->getValue('b', 'bravo'));
+    }
+
+    function testValueFromIndex() {
+        $parser = new CliArgParser(array('script.php', 'foo', 'bar'));
+
+        $this->assertEquals('foo', $parser->getValue(1));
+        $this->assertEquals('bar', $parser->getValue(2));
+        $this->assertEquals('foo', $parser->getValue(-2));
+        $this->assertEquals('bar', $parser->getValue(-1));
+    }
+
+    function testValueFromInvalidIndex() {
+        $parser = new CliArgParser(array('script.php', 'foo', 'bar'));
+        $error = false;
+
+        try {
+            $parser->getValue(3);
+        } catch (InvalidArgumentException $e) {
+            $error = true;
+        }
+
+        $this->assertTrue($error);
+
+        $error = false;
+
+        try {
+            $parser->getValue(-4);
+        } catch (InvalidArgumentException $e) {
+            $error = true;
+        }
+
+        $this->assertTrue($error);
+    }
+
+    function testExtraArguments() {
+        $parser = new CliArgParser(array('script.php', '-f', 'foo', '-b', 'bar', '--', '--abc'));
+
+        $this->assertEquals('foo', $parser->getValue('f'));
+        $this->assertEquals('bar', $parser->getValue('b'));
+        $this->assertEquals(null, $parser->getValue(null, 'abc'));
+        $this->assertEquals(1, sizeof($parser->getExtraArgs()));
+        $this->assertEquals(array('--abc'), $parser->getExtraArgs());
+    }
+
+}
